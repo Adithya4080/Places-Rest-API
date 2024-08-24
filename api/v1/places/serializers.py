7eb11c41e712b6382dbd54881f54cd5082ebb746
adithya4080 <adithyas712@gmail.com>
2024-08-24 10:31:21 +0530

    first commit

---
+++ api/v1/places/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer
+from places.models import Place
+from rest_framework import serializers
+
+
+class PlaceSerializer(ModelSerializer):
+    class Meta:
+        fields = ("id", "name", "featured_image", "place")
+        model = Place
+
+class PlaceDetailSerializer(ModelSerializer):
+
+    category = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ("id", "name", "featured_image", "place", "description", "category")
+        model = Place
+
+    def get_category(self, instance):
+        return instance.category.name
---
+++ api/v1/places/views.py
@@ -0,0 +1,45 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api.v1.places.serializers import PlaceSerializer, PlaceDetailSerializer
+from places.models import Place
+
+
+@api_view(["GET"])
+def places(request):
+    instances = Place.objects.filter(is_deleted=False)
+    context = {
+        "request":request
+    }
+    serializer = PlaceSerializer(instances, many=True, context=context)
+    response_data = {
+        "status_code": 6000,
+        "data": serializer.data
+    }
+
+    return Response(response_data)
+
+
+@api_view(["GET"])
+def place(request,pk):
+    if Place.objects.filter(pk=pk).exists():
+        instance = Place.objects.get(pk=pk)
+
+        context = {
+            "request":request
+        }
+        serializer = PlaceDetailSerializer(instance, context=context)
+        response_data = {
+            "status_code": 6000,
+            "data": serializer.data
+        }
+
+        return Response(response_data)  
+      
+    else:    
+        response_data = {
+            "status_code": 6001,
+            "message": "Place not exist"
+        }
+
+        return Response(response_data)
